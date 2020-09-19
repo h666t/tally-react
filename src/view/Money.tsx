7b4a7e835d291b54334a5a8b63cb6634aa9fbdeb
h666t <1845720928@qq.com>
2020-09-19 20:31:52 +0800

    complete taglist component

--- src/view/Money.tsx
+++ src/view/Money.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {NavLink,useHistory} from 'react-router-dom';
 import {Category} from '../components/Category';
+import {Tags} from './Money/Tags';
 
 const Money:React.FC = () => {
   return (
       <div>
         <Category/>
+        <Tags/>
       </div>
   )
 }
---
+++ src/view/Money/Tags.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Icon} from '../../components/Icon';
+
+const TagList = styled.div`
+    height: 160px;
+    background: white;
+    padding: 15px;
+    overflow: auto;
+    > div{
+      margin-left: -10px;
+      >span{
+        padding: 5px 15px;
+        margin-left: 10px;
+        line-height: 40px;
+        position: relative;
+        display: inline-block;
+        .setting{
+        position: absolute;
+          height: 30px;
+          width: 16px;
+          top: 50%;
+          transform: translateY(-50%);
+        }
+      }
+    
+    }
+`
+
+const Tags:React.FC = ()=>{
+  return (
+    <TagList>
+        <div>
+            <span>衣aaaaaaaaaaaa</span>
+            <span>食</span>
+            <span>住</span>
+            <span>行</span>
+            <span>衣</span>
+            <span>食</span>
+            <span>住</span>
+            <span>行</span>
+          <span>住</span>
+          <span>
+            <Icon id={'#setting'} prefix={'setting'}/>
+          </span>
+
+        </div>
+
+    </TagList>
+  )
+}
+
+export {Tags}
