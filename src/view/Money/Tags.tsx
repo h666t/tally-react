b04be1c21a31eeacbbef35a2b0fe5ee9cb38e3bb
h666t <1845720928@qq.com>
2020-10-01 20:09:11 +0800

    can selecte year for echart in YearScreen component

--- src/view/Money/Tags.tsx
+++ src/view/Money/Tags.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import styled from 'styled-components';
 import {Icon} from '../../components/Icon';
 import {useTags} from '../../hook/useTags';
--- src/view/statistics/YearScreen.tsx
+++ src/view/statistics/YearScreen.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import {DataSourceItem} from '../Money';
 
 const YearScreenWrapper = styled.div`
   height: 200px;
+  background: white;
+  padding-top: 20px;
+  >.preData{
+    display: flex;
+    justify-content: center;
+  }
+  >.years{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    margin-top: 5px;
+    >li{
+      padding: 3px 10px;
+      margin-left: 5px;
+      margin-right: 5px;
+      margin-top: 15px;
+    }
+    >.selected{
+        background: #333333;
+        color: white;
+        border-radius: 15px;
+    }
+  }
 `
 
 const YearScreen:React.FC = () => {
@@ -24,11 +47,17 @@ const YearScreen:React.FC = () => {
       }
     })
   })
+  const [beSelectedYears,setBeSelectedYears] = useState<string>('')
+
   return (
     <YearScreenWrapper>
-      <span>往年数据</span>
+      <div className={'preData'}>往年数据</div>
       <ol className={'years'}>
-        {yearsContainer.map(item=><li key={item}>{item}</li>)}
+        {yearsContainer.map(item=><li key={item} onClick={(event)=>{
+          setBeSelectedYears((event.target as HTMLLIElement).innerText )
+        }}
+        className={beSelectedYears === item ? 'selected' : ''}
+        >{item}</li>)}
       </ol>
     </YearScreenWrapper>
   )
